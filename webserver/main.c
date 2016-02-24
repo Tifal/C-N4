23aa337b7b9c4c51b17d2f3021719cbae21f48a0
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2016-02-24 13:56:29 +0100

    Ajout des primitives fgets,fprintf

--- webserver/main.c
+++ webserver/main.c
@@ -33,6 +33,7 @@ int main ()
 	    break;
 	  }
 	  }*/
+	
 	traiter_client(client);
      
 	close(client);
@@ -44,19 +45,22 @@ int main ()
 }
 void traiter_client(int client){
   char buffer[1024];
+  FILE* file=fdopen(client,"w+");
+   /*On peut maintenant dialoguer avec le client*/
+   /*const char* message="Salut poto comment tu vas ? Moi je payze dans le milieu je suis un mec bien et je suis avec mon poto Fitoussi, un bon gars assez sympa, même si il a une religion assez bizarre mais je crois pas que ce soit sa faute, on choisit pas tout dans la vie. Sinon moi j'suis juste un petit caca qui cherche à s'amuser en faisant le con sur le PC des autres, genre je m'amuse beaucoup très fort, et un jour et bah même que j'en ai fait rager plusieurs en une fois et que j'éatais trop content de moi (RIEN A FOUTRE DES FAUTES DE FRAPPE), et bah même que cette phrase et beaucoup trop longue et qu'il faudrait que je pense à mettre un point. Voilà. Maintenant du coup vu que t'es arrivé sur notre serveur et bah on va bien s'amuser vu qu'on a récupéré ton adresse IP et tout plein d'autres infos sur toi, et même que je vais pouvoir m'amuser à jouer avec ton PC. Sinon on t'as déjà parlé de la otoute puissance du Christ cosmique ? Non ? C'est normal on y connait rien, mais bon. J'AI BESOIN DE CAFE AAAAAAAAAAAH.";
+    */
 while(strcmp("FIN\n",buffer)!=0){	
 	  int i = 0;
-	  if((i=read(client,buffer,sizeof(buffer)))==-1){
-	    perror("read");
+	  // if((i=read(client,buffer,sizeof(buffer)))==-1){
+	  if((i=strlen(fgets(buffer,sizeof(buffer),file)))==-1){
+	    perror("fgets");
 	    break;
 	  }
 	
 	  if(i==0)break;
-	
-	  buffer[i]='\0';
-	
-	  if(write(client,buffer,i)==-1){
-	    perror("write");
+		
+	  if(fprintf(file,"%s %s","<PINKY>",buffer)==-1){
+	    perror("fprintf");
 	    break;
 	  }
 	}
